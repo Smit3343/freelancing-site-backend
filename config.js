9e2099e2a9210e9f3a140d9d485dd4449889fc25
Smit3343 <smitvaghani3343@gmail.com>
2023-09-30 15:50:00 +0530

    enviroments added

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://admin-smit:admin-smit@cluster0.yiyvcil.mongodb.net/',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
     TOKEN_SECRET: process.env.TOKEN_SECRET || 'sjshjwqip!&*sjj!@#1%$&dnnxsk^(ueujdmxwk',
     LISTEN_PORT: process.env.LISTEN_PORT || 4000
 }
